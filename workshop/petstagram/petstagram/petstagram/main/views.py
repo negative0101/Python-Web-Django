7b11ead196846ab66e63d5b1b433edb608b93a4d
negative0101 <negativeoxide@gmail.com>
2022-02-06 15:53:50 +0200

    workshop1.1

--- workshop/petstagram/petstagram/petstagram/main/views.py
+++ workshop/petstagram/petstagram/petstagram/main/views.py
@@ -19,7 +19,7 @@ def show_home(request):
 
 def show_dashboard(request):
     profile = get_profile()
-    pet_photos = set(PetPhoto.objects.filter(tagged_pets__user_profile=profile))
+    pet_photos = set(PetPhoto.objects.prefetch_related('tagged_pets').filter(tagged_pets__user_profile=profile))
     context = {
         'pet_photos': pet_photos,
 
@@ -32,11 +32,14 @@ def show_profile(request):
 
 
 def show_pet_photo_details(request, pk):
-    pet_photo = PetPhoto.objects.get(pk=pk)
+    pet_photo = PetPhoto.objects.prefetch_related('tagged_pets').get(pk=pk)
     context = {
         'pet_photo':pet_photo
     }
     return render(request, 'photo_details.html',context)
 
 def like_pet_photo(request,pk):
+    pet_photo=PetPhoto.objects.get(pk=pk)
+    pet_photo.likes +=1
+    pet_photo.save()
     return redirect('pet photo details', pk)
