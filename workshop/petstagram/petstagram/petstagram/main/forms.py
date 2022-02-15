ea5c6b4ec3f7be4398dc3b6c0ef54c04d3d0ae38
negative0101 <negativeoxide@gmail.com>
2022-02-15 19:53:15 +0200

    workshop

--- workshop/petstagram/petstagram/petstagram/main/forms.py
+++ workshop/petstagram/petstagram/petstagram/main/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
-from petstagram.main.helpers import BootstrapFormMixin
-from petstagram.main.models import Profile
+from petstagram.main.helpers import BootstrapFormMixin, DisabledFieldsFormMixin
+from petstagram.main.models import Profile, PetPhoto, Pet
 
 
 class CreateProfileForm(BootstrapFormMixin, forms.ModelForm):
@@ -69,5 +69,67 @@ class EditProfileForm(BootstrapFormMixin, forms.ModelForm):
                     'rows': 3,
                 }
             ),
+            'date_of_birth': forms.DateInput(
+                attrs={
+                    'min': '1920-01-01'
+                }
+            )
 
         }
+
+
+class DeleteProfileForm(forms.ModelForm):
+    def save(self, commit=True):
+        pets = list(self.instance.pet_set.all())
+        PetPhoto.objects.filter(tagged_pets__in=pets).delete()
+
+        self.instance.delete()
+
+        return self.instance
+
+    class Meta:
+        model = Profile
+        exclude = ('first_name', 'last_name', 'picture', 'date_of_birth', 'description', 'gender',
+                   'email')  # fields= () should work too
+
+
+class CreatePetForm(BootstrapFormMixin, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._init_bootstrap_form_controls()
+
+    class Meta:
+        model = Pet
+        fields = ('name', 'type', 'date_of_birth')
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter pet name',
+                }
+            ),
+        }
+
+
+class EditPetForm(BootstrapFormMixin, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._init_bootstrap_form_controls()
+
+    class Meta:
+        model = Pet
+        exclude = ("user_profile",)
+
+
+class DeletePetForm(BootstrapFormMixin,DisabledFieldsFormMixin, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._init_bootstrap_form_controls()
+        self._init__disabled_fields()
+
+    def save(self,commit=True):
+        self.instance.delete()
+        return self.instance
+
+    class Meta:
+        model=Pet
+        exclude = ("user_profile",)
--- workshop/petstagram/petstagram/petstagram/main/views/profiles.py
+++ workshop/petstagram/petstagram/petstagram/main/views/profiles.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
-from petstagram.main.forms import CreateProfileForm, EditProfileForm
-from petstagram.main.models import Pet, PetPhoto
+from petstagram.main.forms import CreateProfileForm, EditProfileForm, DeleteProfileForm
+from petstagram.main.models import Pet, PetPhoto, Profile
 from petstagram.main.helpers import get_profile
 
 
@@ -18,40 +18,33 @@ def show_profile(request):
         'profile': get_profile,
         'total_likes_count': total_likes_count,
         'total_pet_photos_count': total_pet_photos_count,
+        'pets':pets,
     }
     return render(request, 'profile_details.html', context)
 
 
-def create_profile(request):
+def profile_action(request, form_class, success_url, instance, template_name):
     if request.method == 'POST':
-        form = CreateProfileForm(request.POST)
-        if form.is_valid():
+        form = form_class(request.POST, instance=instance)
+        if form.is_valid:
             form.save()
-            return redirect('index')
+            return redirect(success_url)
     else:
-        form = CreateProfileForm()
+        form = form_class(instance=instance)
+
     context = {
         'form': form,
     }
-    return render(request, 'profile_create.html', context)
+    return render(request, template_name, context)
+
+
+def create_profile(request):
+    return profile_action(request, CreateProfileForm, 'index', Profile(), 'profile_create.html')
 
 
 def edit_profile(request):
-    profile=get_profile()
-    if request.method == 'POST':
-        form = EditProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            return redirect('profile details')
-    else:
-        form = EditProfileForm(instance=profile)
-
-    context = {
-        'form':form
-
-    }
-    return render(request, 'profile_edit.html',context)
+    return profile_action(request, EditProfileForm, 'profile details', get_profile(), 'profile_edit.html')
 
 
 def delete_profile(request):
-    return render(request, 'profile_delete.html')
+    return profile_action(request, DeleteProfileForm, 'index', get_profile(),'profile_delete.html')
