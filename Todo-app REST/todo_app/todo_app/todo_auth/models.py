9c391e5e6c192582ae467f6b1a468777bcbbdd31
negative0101 <negativeoxide@gmail.com>
2022-04-10 16:13:06 +0300

    models

--- Todo-app REST/todo_app/todo_app/todo_auth/models.py
+++ Todo-app REST/todo_app/todo_app/todo_auth/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+UserModel =get_user_model()
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=15, )
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    state = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,)
+    user = models.ForeignKey(UserModel,on_delete=models.CASCADE)
--- Todo-app REST/todo_app/todo_app/todos/admin.py
+++ Todo-app REST/todo_app/todo_app/todos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from todo_app.todo_auth.models import Todo, Category
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title','user__username','is_done')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
